0ba7087e46f86596f95b59a4325e04baeb2484f4
killerman <317309516@qq.com>
2020-01-05 10:35:14 +0800

    complete project with springcloud config

--- config/src/main/java/com/fan/neworderconfig/NeworderConfigApplication.java
+++ config/src/main/java/com/fan/neworderconfig/NeworderConfigApplication.java
@@ -2,8 +2,12 @@ package com.fan.neworderconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
 public class NeworderConfigApplication {
 
 	public static void main(String[] args) {
