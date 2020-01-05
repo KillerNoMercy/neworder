eb3cb6981f768b8ca5ba1cdcac09d606941ce442
killerman <317309516@qq.com>
2020-01-05 10:58:14 +0800

    add admin

---
+++ admin/src/test/java/com/fan/neworderadmin/NeworderAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.neworderadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeworderAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
